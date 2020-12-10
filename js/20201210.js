671ce7d6b75cb77fc6dd7df652f053132470c438
kazutak-x260 <kazutak@outlook.com>
2020-12-10 23:40:24 +0900

    20201210_5

--- js/20201210.js
+++ js/20201210.js
@@ -12,10 +12,22 @@
 //データセットをcsvから読み込む
 // d3.csv("data.csv").then(function(data) {
 //d3.csv("data/mydata.csv").then(function(data){
-d3.csv("data/mydata.csv", function(error, data){
+
+d3.csv("../mydata.csv", function(data) {
+    console.log("This should be displayed first.");
+    console.log(data[0]); // print out the first row of the data
+});
+
+d3.csv("/../mydata.csv", function(data) {
+    console.log("This should be displayed second.");
+    console.log(data[0]); // print out the first row of the data
+})
+
+d3.csv("/data/mydata.csv", function(error, data){
     var dataSet = [];
     for(var i = 0; i < data.length; i++){
         dataSet.push(data[i].item1);
+        console.log('This should be displayd third.');
         console.log(data);
     };
 
