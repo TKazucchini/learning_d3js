4a14fbfeef3fb3bf6325afb7cb90c7e1cb3f1add
kazutak-x260 <kazutak@outlook.com>
2020-12-10 21:39:57 +0900

    first_commit

---
+++ js/20201210.js
@@ -0,0 +1,57 @@
+// データセット
+//var dataSet = [200, 120, 300, 60, 480]
+
+// データセットから1つだけ描画
+//d3.select("#myGraph")
+//.append("rect")
+//.attr("x", 0)
+//.attr("y", 0)
+//.attr("width", dataSet[0])
+//.attr("height", "20px")
+
+//データセットをcsvから読み込む
+// d3.csv("data.csv").then(function(data) {
+//d3.csv("data/mydata.csv").then(function(data){
+d3.csv("data/mydata.json", function(error, data){
+    var dataSet = [];
+    for(var i = 0; i < data.length; i++){
+        dataSet.push(data[i].item1);
+        console.log(data);
+    };
+
+    // データセットを反復して描画
+    d3.select("#myGraph")
+    .selectAll("rect")
+    .data(dataSet)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", function(d, i){
+        return i * 25;
+    })
+    .attr("width", function(d, i){
+        return d + "px";
+    })
+    .attr("height", "20px")
+    .on("click", function(){
+        d3.select(this)
+        .style("fill", "cyan")
+    })
+});
+
+// ボタンクリック時の処理
+d3.select("#updateButton")
+.on("click", function(){
+    // データをランダムに生成
+    for (var i = 0; i < dataSet.length; i++){
+        dataSet[i] = Math.floor(Math.random() * 320) // 0～320未満の数値を生成
+    }
+    d3.select("#myGraph")
+    .selectAll("rect")
+    .data(dataSet)
+    .transition()
+    .duration(500)
+    .attr("width", function(d, i){
+        return d + "px";
+    })
+});
