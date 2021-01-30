bd99920647c002ace6f5999a24b012ce7bd00d1e
kazutak-x260 <kazutak@outlook.com>
2021-01-30 21:41:41 +0900

    20210130_2

--- js/timeSeries_csv.js
+++ js/timeSeries_csv.js
@@ -16,52 +16,100 @@ function drawTimeSeries(data) {
     var svg = d3.select("#timeSeries"),
     margin = {top: 5, right: -30, bottom: 5, left: 60},
     width = +document.getElementsByClassName('time-series')[0].clientWidth - margin.left - margin.right,
-    height = +document.getElementsByClassName('time-series')[0].clientHeight - margin.top - margin.bottom;
+    height = +document.getElementsByClassName('time-series')[0].clientHeight - margin.top - margin.bottom - 80,
+    height2 = 40,
+    radius = (Math.min(width, height) / 2) - 10,
+    node
 
+    var focus = svg.append('g')
+        .attr('class', 'focus')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+    var context = svg.append('g')
+            .attr('class', 'context')
+            .attr('transform', `translate(${margin.left}, ${margin.top + 550})`)
+
+
+    // the date range of available data:
+    var dataXrange = d3.extent(data, d => d.date);
+
+    // maximum date range allowed to display
+    var mindate = dataXrange[0],  // use the range of the data
+        maxdate = dataXrange[1];
+    
     var x = d3.scaleTime()
-    .rangeRound([margin.left, width - margin.right])
-    .domain(d3.extent(data, d => d.date))
-
+            .rangeRound([margin.left, width - margin.right])
+            .domain(dataXrange)
+    var x2 = d3.scaleTime()
+            .rangeRound([margin.left, width - margin.right])
+            .domain([mindate, maxdate])
     var y = d3.scaleLinear()
-    .rangeRound([height - margin.bottom, margin.top]);
-
+            .rangeRound([height - margin.bottom, margin.top]);
+    var y2 = d3.scaleLinear()
+            .rangeRound([height2 - margin.bottom, margin.top]);
     var z = d3.scaleOrdinal(d3.schemeCategory10);
 
+    let xBand = d3.scaleBand().domain(d3.range(-1, data.length)).range([0, width])
+
+
+    // === tick/date formatting functions ===
+    // from: https://stackoverflow.com/questions/20010864/d3-axis-labels-become-too-fine-grained-when-zoomed-in
+
+    function timeFormat(formats) {
+        return function(date) {
+          var i = formats.length - 1, f = formats[i];
+          while (!f[1](date)) f = formats[--i];
+          return f[0](date);
+        };
+      };
+      
+      
+      // 20210124作業メモ  何で xTicks でない？なぜかすべて 1970 になっているよ。
+      
+      var dynamicDateFormat = timeFormat([
+          [d3.timeFormat("%Y"), function() { return true; }],// <-- how to display when Jan 1 YYYY
+          [d3.timeFormat("%b %Y"), function(d) { return d.getMonth(); }],
+          [function(){return "";}, function(d) { return d.getDate() != 1; }]
+      ]);
+
+
     var line = d3.line()
-    //.curve(d3.curveCardinal)
-    .x(d => x(d.date))
-    .y(d => y(d.people));
+        //.curve(d3.curveCardinal)
+        .x(d => x(d.date))
+        .y(d => y(d.people));
+
+
 
     svg.append("g")
-    .attr("class","x-axis")
-    .attr("transform", "translate(0," + (height - margin.bottom) + ")")
-    .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%Y")));
+        .attr("class","x-axis")
+        .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+        .call(d3.axisBottom(x).tickFormat(dynamicDateFormat));
 
     svg.append("g")
-    .attr("class", "y-axis")
-    .attr("transform", "translate(" + margin.left + ",0)");
+        .attr("class", "y-axis")
+        .attr("transform", "translate(" + margin.left + ",0)");
 
     var focus = svg.append("g")
-    .attr("class", "focus")
-    .style("display", "none");
+        .attr("class", "focus")
+        .style("display", "none");
 
     focus.append("line").attr("class", "lineHover")
-    .style("stroke", "#999")
-    .attr("stroke-width", 1)
-    .style("shape-rendering", "crispEdges")
-    .style("opacity", 0.5)
-    .attr("y1", -height)
-    .attr("y2",0);
+        .style("stroke", "#999")
+        .attr("stroke-width", 1)
+        .style("shape-rendering", "crispEdges")
+        .style("opacity", 0.5)
+        .attr("y1", -height)
+        .attr("y2",0);
 
     focus.append("text").attr("class", "lineHoverDate")
-    .attr("text-anchor", "middle")
-    .attr("font-size", 12);
+        .attr("text-anchor", "middle")
+        .attr("font-size", 12);
 
     var overlay = svg.append("rect")
-    .attr("class", "overlay")
-    .attr("x", margin.left)
-    .attr("width", width - margin.right - margin.left)
-    .attr("height", height)
+        .attr("class", "overlay")
+        .attr("x", margin.left)
+        .attr("width", width - margin.right - margin.left)
+        .attr("height", height)
 
 
     var keys = data.columns.slice(1);
